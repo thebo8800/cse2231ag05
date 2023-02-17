26391f7a28b8a4a24f0752821f1bf4051313afcb
thebo8800 <thebo8800@gmail.com>
2023-02-16 23:45:44 -0500

    Completed the bodies of the createNewRep, isInTree, insertInTree, removeSmallest, and removeFromTree private methods, of the (no-argument) constructor, and of the kernel methods (add, remove, removeAny, contains, and size) in Set3a

--- SetOnBST/src/Set3a.java
+++ SetOnBST/src/Set3a.java
@@ -1,6 +1,7 @@
 import java.util.Iterator;
 
 import components.binarytree.BinaryTree;
+import components.binarytree.BinaryTree1;
 import components.set.Set;
 import components.set.SetSecondary;
 
@@ -8,249 +9,274 @@ import components.set.SetSecondary;
  * {@code Set} represented as a {@code BinaryTree} (maintained as a binary
  * search tree) of elements with implementations of primary methods.
  *
- * @param <T>
- *            type of {@code Set} elements
- * @mathdefinitions <pre>
+ * @param <T> type of {@code Set} elements
+ * @mathdefinitions
+ * 
+ *                  <pre>
  * IS_BST(
  *   tree: binary tree of T
  *  ): boolean satisfies
  *  [tree satisfies the binary search tree properties as described in the
  *   slides with the ordering reported by compareTo for T, including that
  *   it has no duplicate labels]
- * </pre>
+ *                  </pre>
+ * 
  * @convention IS_BST($this.tree)
  * @correspondence this = labels($this.tree)
  *
- * @author Put your name here
+ * @author Junbo Chen, Brett Emory
  *
  */
 public class Set3a<T extends Comparable<T>> extends SetSecondary<T> {
 
-    /*
-     * Private members --------------------------------------------------------
-     */
+	/*
+	 * Private members --------------------------------------------------------
+	 */
 
-    /**
-     * Elements included in {@code this}.
-     */
-    private BinaryTree<T> tree;
+	/**
+	 * Elements included in {@code this}.
+	 */
+	private BinaryTree<T> tree;
 
-    /**
-     * Returns whether {@code x} is in {@code t}.
-     *
-     * @param <T>
-     *            type of {@code BinaryTree} labels
-     * @param t
-     *            the {@code BinaryTree} to be searched
-     * @param x
-     *            the label to be searched for
-     * @return true if t contains x, false otherwise
-     * @requires IS_BST(t)
-     * @ensures isInTree = (x is in labels(t))
-     */
-    private static <T extends Comparable<T>> boolean isInTree(BinaryTree<T> t,
-            T x) {
-        assert t != null : "Violation of: t is not null";
-        assert x != null : "Violation of: x is not null";
+	/**
+	 * Returns whether {@code x} is in {@code t}.
+	 *
+	 * @param <T> type of {@code BinaryTree} labels
+	 * @param t   the {@code BinaryTree} to be searched
+	 * @param x   the label to be searched for
+	 * @return true if t contains x, false otherwise
+	 * @requires IS_BST(t)
+	 * @ensures isInTree = (x is in labels(t))
+	 */
+	private static <T extends Comparable<T>> boolean isInTree(BinaryTree<T> t, T x) {
+		assert t != null : "Violation of: t is not null";
+		assert x != null : "Violation of: x is not null";
 
-        // TODO - fill in body
+		boolean inTree = false;
 
-        // This line added just to make the component compilable.
-        return false;
-    }
+		BinaryTree<T> left = t.newInstance();
+		BinaryTree<T> right = t.newInstance();
+		if (t.height() > 0) {
+			T root = t.disassemble(left, right);
+			if (x.equals(root)) {
+				inTree = true;
+			} else if (x.compareTo(root) < 0) {
+				inTree = isInTree(left, x);
+			} else {
+				inTree = isInTree(right, x);
+			}
+			t.assemble(root, left, right);
+		}
+		return inTree;
+	}
 
-    /**
-     * Inserts {@code x} in {@code t}.
-     *
-     * @param <T>
-     *            type of {@code BinaryTree} labels
-     * @param t
-     *            the {@code BinaryTree} to be searched
-     * @param x
-     *            the label to be inserted
-     * @aliases reference {@code x}
-     * @updates t
-     * @requires IS_BST(t) and x is not in labels(t)
-     * @ensures IS_BST(t) and labels(t) = labels(#t) union {x}
-     */
-    private static <T extends Comparable<T>> void insertInTree(BinaryTree<T> t,
-            T x) {
-        assert t != null : "Violation of: t is not null";
-        assert x != null : "Violation of: x is not null";
+	/**
+	 * Inserts {@code x} in {@code t}.
+	 *
+	 * @param <T> type of {@code BinaryTree} labels
+	 * @param t   the {@code BinaryTree} to be searched
+	 * @param x   the label to be inserted
+	 * @aliases reference {@code x}
+	 * @updates t
+	 * @requires IS_BST(t) and x is not in labels(t)
+	 * @ensures IS_BST(t) and labels(t) = labels(#t) union {x}
+	 */
+	private static <T extends Comparable<T>> void insertInTree(BinaryTree<T> t, T x) {
+		assert t != null : "Violation of: t is not null";
+		assert x != null : "Violation of: x is not null";
 
-        // TODO - fill in body
+		BinaryTree<T> left = t.newInstance();
+		BinaryTree<T> right = t.newInstance();
 
-    }
+		if (t.size() > 0) {
+			T root = t.disassemble(left, right);
+			if (x.compareTo(root) > 0) {
+				insertInTree(right, x);
+			} else {
+				insertInTree(left, x);
+			}
+			t.assemble(root, left, right);
+		} else {
+			t.assemble(x, left, right);
+		}
 
-    /**
-     * Removes and returns the smallest (left-most) label in {@code t}.
-     *
-     * @param <T>
-     *            type of {@code BinaryTree} labels
-     * @param t
-     *            the {@code BinaryTree} from which to remove the label
-     * @return the smallest label in the given {@code BinaryTree}
-     * @updates t
-     * @requires IS_BST(t) and |t| > 0
-     * @ensures <pre>
-     * IS_BST(t)  and  removeSmallest = [the smallest label in #t]  and
-     *  labels(t) = labels(#t) \ {removeSmallest}
-     * </pre>
-     */
-    private static <T> T removeSmallest(BinaryTree<T> t) {
-        assert t != null : "Violation of: t is not null";
-        assert t.size() > 0 : "Violation of: |t| > 0";
+	}
 
-        // TODO - fill in body
+	/**
+	 * Removes and returns the smallest (left-most) label in {@code t}.
+	 *
+	 * @param <T> type of {@code BinaryTree} labels
+	 * @param t   the {@code BinaryTree} from which to remove the label
+	 * @return the smallest label in the given {@code BinaryTree}
+	 * @updates t
+	 * @requires IS_BST(t) and |t| > 0
+	 * @ensures
+	 * 
+	 *          <pre>
+	 * IS_BST(t)  and  removeSmallest = [the smallest label in #t]  and
+	 *  labels(t) = labels(#t) \ {removeSmallest}
+	 *          </pre>
+	 */
+	private static <T> T removeSmallest(BinaryTree<T> t) {
+		assert t != null : "Violation of: t is not null";
+		assert t.size() > 0 : "Violation of: |t| > 0";
 
-        // This line added just to make the component compilable.
-        return null;
-    }
+		BinaryTree<T> left = t.newInstance();
+		BinaryTree<T> right = t.newInstance();
+		T root = t.disassemble(left, right);
+		T smallest = null;
+		if (left.size() == 1) {
+			smallest = left.replaceRoot(null);
+		} else {
+			removeSmallest(t);
+		}
+		t.inOrderAssemble(root, left, right);
 
-    /**
-     * Finds label {@code x} in {@code t}, removes it from {@code t}, and
-     * returns it.
-     *
-     * @param <T>
-     *            type of {@code BinaryTree} labels
-     * @param t
-     *            the {@code BinaryTree} from which to remove label {@code x}
-     * @param x
-     *            the label to be removed
-     * @return the removed label
-     * @updates t
-     * @requires IS_BST(t) and x is in labels(t)
-     * @ensures <pre>
-     * IS_BST(t)  and  removeFromTree = x  and
-     *  labels(t) = labels(#t) \ {x}
-     * </pre>
-     */
-    private static <T extends Comparable<T>> T removeFromTree(BinaryTree<T> t,
-            T x) {
-        assert t != null : "Violation of: t is not null";
-        assert x != null : "Violation of: x is not null";
-        assert t.size() > 0 : "Violation of: x is in labels(t)";
+		return smallest;
+	}
 
-        // TODO - fill in body
+	/**
+	 * Finds label {@code x} in {@code t}, removes it from {@code t}, and returns
+	 * it.
+	 *
+	 * @param <T> type of {@code BinaryTree} labels
+	 * @param t   the {@code BinaryTree} from which to remove label {@code x}
+	 * @param x   the label to be removed
+	 * @return the removed label
+	 * @updates t
+	 * @requires IS_BST(t) and x is in labels(t)
+	 * @ensures
+	 * 
+	 *          <pre>
+	 * IS_BST(t)  and  removeFromTree = x  and
+	 *  labels(t) = labels(#t) \ {x}
+	 *          </pre>
+	 */
+	private static <T extends Comparable<T>> T removeFromTree(BinaryTree<T> t, T x) {
+		assert t != null : "Violation of: t is not null";
+		assert x != null : "Violation of: x is not null";
+		assert t.size() > 0 : "Violation of: x is in labels(t)";
 
-        // This line added just to make the component compilable.
-        return null;
-    }
+		BinaryTree<T> left = t.newInstance();
+		BinaryTree<T> right = t.newInstance();
+		T root = t.disassemble(left, right);
+		T removeElement = null;
 
-    /**
-     * Creator of initial representation.
-     */
-    private void createNewRep() {
+		if (x.equals(root)) {
+			removeElement = root;
+		} else if (x.compareTo(root) > 0) {
+			removeFromTree(right, x);
+		} else {
+			removeFromTree(left, x);
+		}
 
-        // TODO - fill in body
+		t.inOrderAssemble(root, left, right);
 
-    }
+		return removeElement;
+	}
 
-    /*
-     * Constructors -----------------------------------------------------------
-     */
+	/**
+	 * Creator of initial representation.
+	 */
+	private void createNewRep() {
 
-    /**
-     * No-argument constructor.
-     */
-    public Set3a() {
+		this.tree = new BinaryTree1<T>();
 
-        // TODO - fill in body
+	}
 
-    }
+	/*
+	 * Constructors -----------------------------------------------------------
+	 */
 
-    /*
-     * Standard methods -------------------------------------------------------
-     */
+	/**
+	 * No-argument constructor.
+	 */
+	public Set3a() {
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public final Set<T> newInstance() {
-        try {
-            return this.getClass().getConstructor().newInstance();
-        } catch (ReflectiveOperationException e) {
-            throw new AssertionError(
-                    "Cannot construct object of type " + this.getClass());
-        }
-    }
+		this.createNewRep();
 
-    @Override
-    public final void clear() {
-        this.createNewRep();
-    }
+	}
 
-    @Override
-    public final void transferFrom(Set<T> source) {
-        assert source != null : "Violation of: source is not null";
-        assert source != this : "Violation of: source is not this";
-        assert source instanceof Set3a<?> : ""
-                + "Violation of: source is of dynamic type Set3<?>";
-        /*
-         * This cast cannot fail since the assert above would have stopped
-         * execution in that case: source must be of dynamic type Set3a<?>, and
-         * the ? must be T or the call would not have compiled.
-         */
-        Set3a<T> localSource = (Set3a<T>) source;
-        this.tree = localSource.tree;
-        localSource.createNewRep();
-    }
+	/*
+	 * Standard methods -------------------------------------------------------
+	 */
 
-    /*
-     * Kernel methods ---------------------------------------------------------
-     */
+	@SuppressWarnings("unchecked")
+	@Override
+	public final Set<T> newInstance() {
+		try {
+			return this.getClass().getConstructor().newInstance();
+		} catch (ReflectiveOperationException e) {
+			throw new AssertionError("Cannot construct object of type " + this.getClass());
+		}
+	}
 
-    @Override
-    public final void add(T x) {
-        assert x != null : "Violation of: x is not null";
-        assert !this.contains(x) : "Violation of: x is not in this";
+	@Override
+	public final void clear() {
+		this.createNewRep();
+	}
 
-        // TODO - fill in body
+	@Override
+	public final void transferFrom(Set<T> source) {
+		assert source != null : "Violation of: source is not null";
+		assert source != this : "Violation of: source is not this";
+		assert source instanceof Set3a<?> : "" + "Violation of: source is of dynamic type Set3<?>";
+		/*
+		 * This cast cannot fail since the assert above would have stopped execution in
+		 * that case: source must be of dynamic type Set3a<?>, and the ? must be T or
+		 * the call would not have compiled.
+		 */
+		Set3a<T> localSource = (Set3a<T>) source;
+		this.tree = localSource.tree;
+		localSource.createNewRep();
+	}
 
-    }
+	/*
+	 * Kernel methods ---------------------------------------------------------
+	 */
 
-    @Override
-    public final T remove(T x) {
-        assert x != null : "Violation of: x is not null";
-        assert this.contains(x) : "Violation of: x is in this";
+	@Override
+	public final void add(T x) {
+		assert x != null : "Violation of: x is not null";
+		assert !this.contains(x) : "Violation of: x is not in this";
 
-        // TODO - fill in body
+		insertInTree(this.tree, x);
 
-        // This line added just to make the component compilable.
-        return null;
-    }
+	}
 
-    @Override
-    public final T removeAny() {
-        assert this.size() > 0 : "Violation of: this /= empty_set";
+	@Override
+	public final T remove(T x) {
+		assert x != null : "Violation of: x is not null";
+		assert this.contains(x) : "Violation of: x is in this";
 
-        // TODO - fill in body
+		return removeFromTree(this.tree, x);
+	}
 
-        // This line added just to make the component compilable.
-        return null;
-    }
+	@Override
+	public final T removeAny() {
+		assert this.size() > 0 : "Violation of: this /= empty_set";
 
-    @Override
-    public final boolean contains(T x) {
-        assert x != null : "Violation of: x is not null";
+		return removeSmallest(this.tree);
+	}
 
-        // TODO - fill in body
+	@Override
+	public final boolean contains(T x) {
+		assert x != null : "Violation of: x is not null";
 
-        // This line added just to make the component compilable.
-        return false;
-    }
+		return isInTree(this.tree, x);
 
-    @Override
-    public final int size() {
+	}
 
-        // TODO - fill in body
+	@Override
+	public final int size() {
 
-        // This line added just to make the component compilable.
-        return 0;
-    }
+		return this.tree.size();
 
-    @Override
-    public final Iterator<T> iterator() {
-        return this.tree.iterator();
-    }
+	}
+
+	@Override
+	public final Iterator<T> iterator() {
+		return this.tree.iterator();
+	}
 
 }
